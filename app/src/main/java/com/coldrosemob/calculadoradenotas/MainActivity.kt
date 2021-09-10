a1e8524d7da43794b7cd370ed9a40f76ffd3b7fc
arthu <arthurguilhermesb@gmail.com>
2021-09-10 00:25:40 -0300

    Instantiation of elements of Layout

--- app/src/main/java/com/coldrosemob/calculadoradenotas/MainActivity.kt
+++ app/src/main/java/com/coldrosemob/calculadoradenotas/MainActivity.kt
@@ -2,10 +2,18 @@ package com.coldrosemob.calculadoradenotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val note01_edt: EditText = findViewById(R.id.note01_edt)
+        val note02_edt: EditText = findViewById(R.id.note02_edt)
+        val calculate_btn: Button = findViewById(R.id.calculate_btn)
+
     }
 }
